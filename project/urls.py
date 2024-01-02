77a4b2364356996f72ac6cca44b3f74b559e0191
VladRosi <a3714152241a@gmail.com>
2024-01-02 15:22:24 +0300

    First commit

---
+++ project/urls.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.conf import settings
+from django.urls import path, include
+from accounts.views import logout_view
+
+urlpatterns = [
+  path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+  path('accounts/logout/', logout_view, name='logout'),
+  path("admin/", admin.site.urls),
+  path("__reload__/", include("django_browser_reload.urls")),
+]
+
+if settings.DEBUG:
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
